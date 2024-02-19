9fe3189f8a6e17fb192d7b97bf180707d16ddb1b
x13d <dimasik7557@gmail.com>
2024-02-19 19:36:28 +0300

    I've updated the code a bit

--- app/src/main/java/com/example/terminalioemulator/emulator/JavaEmulator.java
+++ app/src/main/java/com/example/terminalioemulator/emulator/JavaEmulator.java
@@ -4,10 +4,24 @@ import com.example.terminalioemulator.IOEmulator;
 
 public class JavaEmulator {
     public static void mainEmulator(String[] args) {
-        // Write your code here
-        IOEmulator.INSTANCE.println("");
-        IOEmulator.INSTANCE.print(1234);
+        float result = 0;
+        IOEmulator.INSTANCE.println("input your variable a");
+        float var_a = IOEmulator.INSTANCE.read(Float.class);
+        IOEmulator.INSTANCE.println("input your variable b");
+        float var_b = IOEmulator.INSTANCE.read(Float.class);
+        IOEmulator.INSTANCE.println("input /, +, -, *, &");
+        String var_text = IOEmulator.INSTANCE.read(String.class);
+        if (var_text.equals("+")) {
+            result = var_a + var_b;
+        } else if (var_text.equals("-")) {
+            result = var_a - var_b;
+        } else if (var_text.equals("/")) {
+            result = var_a / var_b;
+        }else if (var_text.equals("*")) {
+            result = var_a * var_b;
+        }
+        IOEmulator.INSTANCE.print(result);
 
-        int test = IOEmulator.INSTANCE.read(int.class);
     }
+
 }
